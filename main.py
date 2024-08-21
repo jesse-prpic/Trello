b2a471bdc8c547ea3a38ab4f0450495e3d9c5a02
jesse-prpic <13812@coderacademy.edu.au>
2024-08-21 16:09:01 +1000

    finisheed class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from init import db, ma, bcrypt, jwt
 
 def create_app():
-    app = Flask (__name__)
+    app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
